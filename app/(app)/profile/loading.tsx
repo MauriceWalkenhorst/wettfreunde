b07c8b1c691eabdfabb8b5664239dbc2e8ecc284
mawal017 <maurice.walkenhorst017@stud.fh-dortmund.de>
2026-03-25 12:19:11 +0100

    feat: add loading.tsx spinners to all app routes

---
+++ app/(app)/profile/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center py-20">
+      <div className="w-6 h-6 border-2 border-zinc-300 border-t-zinc-900 rounded-full animate-spin" />
+    </div>
+  )
+}
